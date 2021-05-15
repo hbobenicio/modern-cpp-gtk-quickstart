d33f4752133579a8a55256c2f8a424b2ebf77b0d
hbobenicio <hbobenicio@gmail.com>
2021-05-15 13:59:30 -0300

    minimal gtk with minimal toolbar

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -3,7 +3,18 @@
 namespace app {
     
     MainWindow::MainWindow() {
-        set_title("Basic application");
-        set_default_size(200, 200);
+        set_title("Modern CPP GTK Quickstart");
+        set_default_size(600, 400);
+        set_position(Gtk::WindowPosition::WIN_POS_CENTER);
+
+        import_tool_button.set_icon_name("gtk-open");
+        import_tool_button.set_tooltip_text("Import certificates from site...");
+        toolbar.add(import_tool_button);
+
+        main_vbox.pack_start(toolbar, Gtk::PackOptions::PACK_SHRINK);
+        main_vbox.pack_end(certificates_tree_view);
+
+        add(main_vbox);
+        show_all();
     }
 }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -2,10 +2,19 @@
 #define APP_MAIN_WINDOW_H
 
 #include <gtkmm/window.h>
+#include <gtkmm/toolbar.h>
+#include <gtkmm/box.h>
+#include <gtkmm/treeview.h>
 
 namespace app {
 
     class MainWindow : public Gtk::Window {
+    private:
+        Gtk::VBox main_vbox;
+        Gtk::Toolbar toolbar;
+        Gtk::ToolButton import_tool_button;
+        Gtk::TreeView certificates_tree_view;
+
     public:
         MainWindow();
     };
